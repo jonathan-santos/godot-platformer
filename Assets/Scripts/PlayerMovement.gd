76b8b8955ce325e2b1feeac335d862cfaa4aad3a
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-05-20 01:50:49 -0300

    Criado pasta Assets, inserido sprites de personagem e tilesets, criado tilesets e o inserido na cena stage1 e inserido animação no personagem

--- Assets/Scripts/PlayerMovement.gd
+++ Assets/Scripts/PlayerMovement.gd
@@ -11,13 +11,26 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = VELOCITY
+		$Sprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -VELOCITY
+		$Sprite.flip_h = true
 	else:
 		motion.x = 0
 		
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		motion.y = JUMP_HEIGHT
 		
+	play_animations()
 	motion = move_and_slide(motion, UP)
+	pass
+
+func play_animations():
+	if is_on_floor():
+		if motion.x == 0:
+			$Sprite.play("idle")
+		else:
+			$Sprite.play("run")
+	else:
+		$Sprite.play("jump")
 	pass
